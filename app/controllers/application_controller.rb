7f46f618baf150a735dc77d764892956b78fe5ce
DanFerrer <danielferrer212@gmail.com>
2015-04-14 16:43:10 -0400

    for demo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-require_relative '../decorators/user_decorator.rb'
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
